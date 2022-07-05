11ca98ae79e8d11f4f23a825de787693302860d9
MrLee <PCducla@gmail.com>
2022-07-05 23:20:09 +0700

    quay xe lan 1

--- src/main/java/com/mta/demo/controller/HomeController.java
+++ src/main/java/com/mta/demo/controller/HomeController.java
@@ -4,5 +4,6 @@ public class HomeController {
     public static void main(String[] args) {
         int a = 10;
         System.out.println("hai quay xe!");
+        System.out.println("hai quay xe lan 3!");
     }
 }
