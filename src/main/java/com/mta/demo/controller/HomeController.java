0f842a78bf7847099d9ae6f1865c20100e95775b
MrLee <PCducla@gmail.com>
2022-07-05 23:14:19 +0700

    quay xe

--- src/main/java/com/mta/demo/controller/HomeController.java
+++ src/main/java/com/mta/demo/controller/HomeController.java
@@ -3,5 +3,6 @@ package com.mta.demo.controller;
 public class HomeController {
     public static void main(String[] args) {
         int a = 10;
+        System.out.println("hai quay xe!");
     }
 }
